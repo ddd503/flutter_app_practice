aab312eb936861919d61b33e0b92e0d3496f19a2
ddd503 <skbsporta7@gmail.com>
2019-05-25 15:59:41 +0900

    test layout

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -11,18 +10,41 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter やってみた'),
+      home: MyHomePage(title: 'Flutter やってみた', text: 'テストテキスト')
+    );
+  }
+}
+
+// ignore: unused_element
+class _TestView extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Center(child: Container(
+      width: width,
+      height: height,
+      color: Colors.blue,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:
+        <Widget>[Text(widget.text)],
+      ),
+    ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key, this.title, this.text}) : super(key: key);
 
   final String title;
+  final String text;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _TestView createState() => _TestView();
+//  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
