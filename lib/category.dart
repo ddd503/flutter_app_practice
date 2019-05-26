277247f03ee77aee3aa3893409fad6c86a917105
ddd503 <skbsporta7@gmail.com>
2019-05-26 18:56:55 +0900

    add custom widget

---
+++ lib/category.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
+class Category extends StatelessWidget {
+  // Propaty
+  final String name;
+  final ColorSwatch color;
+  final IconData iconLocation;
+
+  // nonNullのイニシャライズ
+  const Category({
+    Key key,
+    @required this.name,
+    @required this.color,
+    @required this.iconLocation,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(iconLocation != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: color,
+          splashColor: color,
+          // We can use either the () => function() or the () { function(); }
+          // syntax.
+          onTap: () {
+            print('I was tapped!');
+          },
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              // There are two ways to denote a list: `[]` and `List()`.
+              // Prefer to use the literal syntax, i.e. `[]`, instead of `List()`.
+              // You can add the type argument if you'd like, i.e. <Widget>[].
+              // See https://www.dartlang.org/guides/language/effective-dart/usage#do-use-collection-literals-when-possible
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(
+                    iconLocation,
+                    size: 60.0,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_practice/category.dart';
 
 void main() => runApp(MyApp());
 
 const _padding = EdgeInsets.all(16.0);
+const _categoryName = 'Cake';
+const _categoryIcon = Icons.cake;
+const _categoryColor = Colors.green;
 
 // 起動時の画面(homeの値は必須)
 class MyApp extends StatelessWidget {
@@ -18,26 +22,9 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Hello Warld'),
           backgroundColor: Colors.green,),
-        body: HelloWarldRactangle(),)
-    );
-  }
-}
-
-class HelloWarldRactangle extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 300,
-        height: 400,
-        color: Colors.green,
-        alignment: Alignment.center,
-        child: Text(
-          'Hello!',
-          style: Theme.of(context).textTheme.display2,
-        ),
-      ),
+        backgroundColor: Colors.yellow[100],
+        body: Category(name: _categoryName, color: _categoryColor, iconLocation: _categoryIcon),
+      )
     );
   }
 }
